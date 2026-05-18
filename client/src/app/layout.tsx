63cf92951de0035fee0ee33d450856beaa3526b4
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2026-05-18 11:52:50 -0400

    feat: update application title and description to reflect new branding as BMetrics OPC; enhance metadata with relevant keywords for improved SEO

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,9 +15,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "BMetrics",
+  title: "BMetrics OPC",
   description:
-    "BMetrics is a dynamic and intuitive software application designed to track and analyze behavior data, specifically in the context of Applied Behavior Analysis (ABA)...",
+    "BMetrics OPC is a OPC Behavior Consulting dynamic and intuitive software application designed to track and analyze behavior data, specifically in the context of Applied Behavior Analysis (ABA). Aimed at behavior analysts, educators, and clinicians, BMetrics helps users efficiently monitor various behavioral metrics, such as frequency, duration, and rate, to assess intervention efficacy and track behavioral trends over time. The platform allows users to visualize their data through interactive graphs, with customizable date ranges and measurement types. With a focus on user-friendliness and flexibility, BMetrics offers tools for data aggregation, reporting, and handling missing data, making it an essential tool for professionals in the field of behavior analysis.",
+  keywords: ["OPC Behavior Consulting LLC", "OPC Behavior Consulting BMetrics", "OPC BMetrics", "OPC Metrics", "OPC Behavior Consulting", "OPC Behavior Metrics", "OPC BMetrics", "OPC Metrics", "behavior analysis", "applied behavior analysis", "ABA", "behavior monitoring", "behavior tracking", "behavior data", "behavior analysis software", "behavior analysis tools", "behavior analysis reports", "behavior analysis graphs", "behavior analysis charts", "behavior analysis data visualization", "behavior analysis data reporting", "behavior analysis data handling", "behavior analysis data missing data", "behavior analysis data aggregation", "behavior analysis data reporting", "behavior analysis data handling", "behavior analysis data missing data", "behavior analysis data aggregation"],
   icons: {
     icon: "/BMetrics-logo-removebg.ico",
   },
